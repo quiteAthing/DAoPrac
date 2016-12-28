15056bc0b39b294a24402721cddd740ac690daae
PC-26 <PC-26@PC-26>
2016-12-28 16:03:06 +0800

    解決掉無法傳中文的問題。

--- src/petts/MainPet.java
+++ src/petts/MainPet.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainPet {
@@ -17,14 +18,14 @@ public class MainPet {
 		
 //		 本程式新增內容的標準做法
 //		  Map<String,String> data=new HashMap();
-//		  data.put("petName?"," \"  Honey bee \" ");
-//		  data.put("masterName?"," \" Winny the pooh \" ");
+//		  data.put("petName?"," \"  黑白切 \" ");
+//		  data.put("masterName?"," \" 滷味 \" ");
 //		  data.put("birthday?"," 19450622 ");
 //		  data.put("price?"," 399999512 ");
 //		  data.put("weight?"," 359.2 ");
-//		  data.put("filename?"," \"  pet (4).jpg\" "); File pic=new
-//		  File("D:\\daoResource\\dao (4).jpg"); File comment=new
-//		  File("D:\\daoResource\\pet (4).txt");
+//		  data.put("filename?"," \"  pet (2).jpg\" "); File pic=new
+//		  File("D:\\daoResource\\dao (2).jpg"); File comment=new
+//		  File("D:\\daoResource\\pet (2).txt");
 //		  accessor.insert(data,pic,comment);
 		 
 
@@ -44,8 +45,9 @@ public class MainPet {
 
 //		
 //		 刪除的標準做法(這隻程式only) 這邊偷懶只允許用id刪除。基本上只要傳id即可。 連模板也跟其他不一樣。
-//		 accessor.delete(2);
-//		
+//		 accessor.delete(7);
+//		 accessor.delete(8);
+		
 		
 		
 //		 這個程式裡查詢資料的標準做法
@@ -82,14 +84,14 @@ public class MainPet {
 //		for(petDAO pd : petlist){
 //			pd.getDataInside();
 //		}
-//		
+		
 
 		
 	}
 
 	public static Connection connect() {
 		try {
-			Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1/course?useSSL=true&useUnicode=true",
+			Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1/course?useSSL=true&useUnicode=true&characterEncoding=UTF-8",
 					"root", "MyNewPass");
 			return c;
 		} catch (SQLException e) {
